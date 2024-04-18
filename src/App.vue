b43cbbfaeba8bee4401c5f426fd1a946de3ecab7
Rahim Uddin <rahimuddin5915@gmail.com>
2024-04-18 12:04:26 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>{{ courseName }}</h1>
+    <h3> Course by {{ mentor }}</h3>
+    <h2>{{ count }}</h2>
+    <button @click="count++" >+</button>
+    <button @click="count--" >-</button>
+</template>
+
+<!-- <script>
+export default{
+    data(){
+        return{
+            courseName:"Vue js 3",
+            mentor:'Rahim Uddin',
+            count:0,
+        }
+    }
+}
+</script> -->
+<!-- <script>
+export default{
+    setup(){
+        let count=0;
+        const courseName="Vue js 3";
+        const mentor="Mentor";
+    return {count, courseName, mentor};
+    }
+}
+</script> -->
+<script setup>
+import { ref } from 'vue';
+
+        let count= ref(0)
+        const courseName=ref("Vue js 3");
+        const mentor=ref("Mentor");
+</script>
