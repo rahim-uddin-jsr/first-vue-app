220b7f71b1896226ec68e542a5664f84b01ac5d9
Rahim Uddin <rahimuddin5915@gmail.com>
2024-04-21 17:10:35 +0600

    added bootsrat with npm and scss

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import router from "./Router";
 import { createPinia } from "pinia";
 
+import  './assets/scss/styles.scss';
 
 const app=createApp(App);
 const pinia=createPinia();
 app.use(pinia);
 app.use(router).mount('#app');
 
+import * as bootstrap from "bootstrap";
