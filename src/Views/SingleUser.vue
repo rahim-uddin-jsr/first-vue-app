219e6274e06ab7728f1c284a1c6eec8ad13af9e1
Rahim Uddin <rahimuddin5915@gmail.com>
2024-04-21 15:03:08 +0600

    explore userRouter

--- src/Views/SingleUser.vue
+++ src/Views/SingleUser.vue
@@ -1,4 +1,14 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+
+const router=useRouter()
+const goHome=()=>{
+    router.push({name:'home'})
+}
+</script>
 <template>
     <h2>Single User</h2>
     <h6>User Id is {{  $route.params.id }}</h6>
+    <button @click="goHome">Back to Home</button>
 </template>
